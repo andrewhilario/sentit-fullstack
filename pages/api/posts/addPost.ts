5f19bf605912dc28234c7517943b6afb7bb7565e
Drew Hilario <hilarioandrew12@gmail.com>
2023-02-16 01:30:20 +0800

    first commit

--- pages/api/posts/addPost.ts
+++ pages/api/posts/addPost.ts
@@ -1,5 +1,5 @@
 import prisma from "../../../prisma/client"
-import { unstable_getServerSession } from "next-auth/next"
+import { getServerSession } from "next-auth/next"
 import { authOptions } from "../auth/[...nextauth]"
 import type { NextApiRequest, NextApiResponse } from "next"
 
@@ -8,7 +8,7 @@ export default async function handler(
     res: NextApiResponse
 ) {
     if (req.method === "POST") {
-        const session = await unstable_getServerSession(req, res, authOptions)
+        const session = await getServerSession(req, res, authOptions)
         if (!session) {
             return res
                 .status(401)
